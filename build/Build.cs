ac02868d544d33b2bbcd0e67836d0053363777ad
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2023-04-16 14:24:12 +0200

    fix(ci): the `source` parameter is missing when trying to publish nuget package to Github

--- build/Build.cs
+++ build/Build.cs
@@ -6,6 +6,7 @@ namespace Candoumbe.Forms.ContinuousIntegration
     using Candoumbe.Pipelines.Components.Workflows;
 
     using Nuke.Common;
+    using Nuke.Common.CI;
     using Nuke.Common.CI.GitHubActions;
     using Nuke.Common.IO;
     using Nuke.Common.ProjectModel;
@@ -91,6 +92,9 @@ namespace Candoumbe.Forms.ContinuousIntegration
         [Solution]
         [Required]
         public readonly Solution Solution;
+
+        [CI]
+        public GitHubActions GitHubActions;
 
         ///<inheritdoc/>
         Solution IHaveSolution.Solution => Solution;
