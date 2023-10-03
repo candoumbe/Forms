3ebd393542d12d15063f886c970781e35e2242c7
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2023-10-03 23:01:32 +0200

    fix(ci): fix broken ci
    🛠️Fix broken ci
    🧹add stryker configuration file

--- build/Build.cs
+++ build/Build.cs
@@ -3,6 +3,7 @@ namespace Forms.ContinuousIntegration
     using Candoumbe.Pipelines;
     using Candoumbe.Pipelines.Components;
     using Candoumbe.Pipelines.Components.GitHub;
+    using Candoumbe.Pipelines.Components.NuGet;
     using Candoumbe.Pipelines.Components.Workflows;
 
     using Nuke.Common;
@@ -49,7 +50,7 @@ namespace Forms.ContinuousIntegration
         GitHubActionsImage.UbuntuLatest,
         FetchDepth = 0,
         OnPushBranches = new[] { IHaveMainBranch.MainBranchName },
-        InvokedTargets = new[] { nameof(IUnitTest.UnitTests), nameof(IPublish.Publish), nameof(ICreateGithubRelease.AddGithubRelease) },
+        InvokedTargets = new[] { nameof(IUnitTest.UnitTests), nameof(IPushNugetPackages.Publish), nameof(ICreateGithubRelease.AddGithubRelease) },
         EnableGitHubToken = true,
         CacheKeyFiles = new[] { "global.json", "src/**/*.csproj", "test/**/*.csproj" },
         PublishArtifacts = true,
@@ -81,9 +82,12 @@ namespace Forms.ContinuousIntegration
         IBenchmark,
         IReportCoverage,
         IPack,
-        IPublish,
+        IPushNugetPackages,
         ICreateGithubRelease
     {
+        /// <summary>
+        /// ApiKey required to publish packages to NuGet
+        /// </summary>
         [Parameter("API key used to publish artifacts to Nuget.org")]
         [Secret]
         public readonly string NugetApiKey;
@@ -103,20 +107,14 @@ namespace Forms.ContinuousIntegration
             .Concat(this.Get<IHaveTestDirectory>().TestDirectory.GlobDirectories("**/bin", "**/obj"));
 
         ///<inheritdoc/>
-        AbsolutePath IHaveSourceDirectory.SourceDirectory => RootDirectory / "src";
-
-        ///<inheritdoc/>
         AbsolutePath IHaveTestDirectory.TestDirectory => RootDirectory / "tests";
 
 
         ///<inheritdoc/>
-        IEnumerable<Project> IUnitTest.UnitTestsProjects => this.Get<IHaveSolution>().Solution.GetProjects("*UnitTests");
+        IEnumerable<Project> IUnitTest.UnitTestsProjects => this.Get<IHaveSolution>().Solution.GetAllProjects("*UnitTests");
 
         ///<inheritdoc/>
-        IEnumerable<Project> IMutationTest.MutationTestsProjects => this.Get<IUnitTest>().UnitTestsProjects;
-
-        ///<inheritdoc/>
-        IEnumerable<Project> IBenchmark.BenchmarkProjects => this.Get<IHaveSolution>().Solution.GetProjects("*.PerfomanceTests");
+        IEnumerable<Project> IBenchmark.BenchmarkProjects => this.Get<IHaveSolution>().Solution.GetAllProjects("*.PerfomanceTests");
 
         ///<inheritdoc/>
         bool IReportCoverage.ReportToCodeCov => this.Get<IReportCoverage>().CodecovToken is not null;
@@ -125,14 +123,21 @@ namespace Forms.ContinuousIntegration
         IEnumerable<AbsolutePath> IPack.PackableProjects => this.Get<IHaveSourceDirectory>().SourceDirectory.GlobFiles("**/*.csproj");
 
         ///<inheritdoc/>
-        IEnumerable<PublishConfiguration> IPublish.PublishConfigurations => new PublishConfiguration[]
+        IEnumerable<PushNugetPackageConfiguration> IPushNugetPackages.PublishConfigurations => new PushNugetPackageConfiguration[]
         {
-            new NugetPublishConfiguration(apiKey: NugetApiKey,
-                                          source: new Uri("https://api.nuget.org/v3/index.json"),
-                                          () => NugetApiKey is not null),
-            new GitHubPublishConfiguration(githubToken: this.Get<IHaveGitHubRepository>().GitHubToken,
-                                           source: new Uri("https://nukpg.github.com/"),
-                                           () => this is ICreateGithubRelease && this.Get<ICreateGithubRelease>()?.GitHubToken is not null)
+            new NugetPushConfiguration(apiKey: NugetApiKey,
+                                       source: new Uri("https://api.nuget.org/v3/index.json"),
+                                       () => NugetApiKey is not null),
+            new GitHubPushNugetConfiguration(githubToken: this.Get<IHaveGitHubRepository>().GitHubToken,
+                                             source: new Uri("https://nukpg.github.com/"),
+                                             () => this is ICreateGithubRelease && this.Get<ICreateGithubRelease>()?.GitHubToken is not null)
+        };
+
+        ///<inheritdoc/>
+        IEnumerable<MutationProjectConfiguration> IMutationTest.MutationTestsProjects => new[]{
+            new MutationProjectConfiguration(sourceProject: Solution.AllProjects.Single(csproj => string.Equals(csproj.Name, "Forms")),
+                                             testProjects: Solution.GetAllProjects("Forms.UnitTests"),
+                                             configurationFile: this.Get<IHaveTestDirectory>().TestDirectory / "Forms.UnitTests" / "stryker-config.json")
         };
 
         ///<inheritdoc/>
